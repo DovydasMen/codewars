11ce2633b1534a39ff60d78078660c1f22312a5c
Dovydas Menkevicius <d.menkevicius@gmail.com>
2023-11-07 16:33:55 +0200

    Add even_or_ood

---
+++ even_or_odd.py
@@ -0,0 +1,10 @@
+def even_or_odd(number):
+    if number == 0:
+        return "Even"
+    if number % 2 != 0:
+        return "Odd"
+    else:
+        return "Even"
+    
+
+print(even_or_odd(-3))
